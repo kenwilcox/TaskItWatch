228e15212a98415c9b6af46e9350d3b9f64ab593
Ken Wilcox <wilcoxk@gmail.com>
2015-04-08 21:33:23 -0600

    added CoreDataShare Framework

---
+++ CoreDataShare/CoreDataShare.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataShare.h
+//  CoreDataShare
+//
+//  Created by Kenneth Wilcox on 4/8/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataShare.
+FOUNDATION_EXPORT double CoreDataShareVersionNumber;
+
+//! Project version string for CoreDataShare.
+FOUNDATION_EXPORT const unsigned char CoreDataShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataShare/PublicHeader.h>
+
+
--- TaskItWatch WatchKit Extension/InterfaceController.swift
+++ TaskItWatch WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,24 @@
 
 import WatchKit
 import Foundation
-
+import CoreDataShare
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
 }
