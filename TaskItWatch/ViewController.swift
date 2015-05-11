f802c51f9c88fde12b89e65718f3261c5aaef1f7
Ken Wilcox <wilcoxk@gmail.com>
2015-05-10 20:04:18 -0600

    Getting the TableView set up

--- TaskItWatch/ViewController.swift
+++ TaskItWatch/ViewController.swift
@@ -13,10 +13,14 @@ import CoreData
 class ViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
+  var fetchedResultsController: NSFetchedResultsController!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
   }
   
   override func didReceiveMemoryWarning() {
@@ -27,5 +31,60 @@ class ViewController: UIViewController {
   @IBAction func addTaskBarButtonItemPressed(sender: UIBarButtonItem) {
     self.performSegueWithIdentifier("toAddTaskSegue", sender: nil)
   }
+
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource
+{
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 0
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate
+{
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    
+  }
+  
+  func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+  }
+  
+  func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    
+  }
+}
+
+// MARK: - FetchedResultsControllerDelegate
+extension ViewController: NSFetchedResultsControllerDelegate
+{
+  func setupFetchedResultsController () {
+    var fetchRequest = NSFetchRequest()
+    var context = CoreDataManager.sharedInstance.managedObjectContext!
+    
+    let entity = NSEntityDescription.entityForName("Task", inManagedObjectContext: context)
+    fetchRequest.entity = entity
+    
+    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+    
+    self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+    
+    self.fetchedResultsController.delegate = self
+    
+    var error: NSError? = nil
+    if !self.fetchedResultsController!.performFetch(&error) {
+      println("An error Occured \(error)")
+    }
+  }
+}
