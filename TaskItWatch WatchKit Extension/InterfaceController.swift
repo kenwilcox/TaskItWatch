b0753bbf9b1e865fdc0ed569687b60b470e25372
Ken Wilcox <wilcoxk@gmail.com>
2015-05-10 21:59:12 -0600

    Getting tasks to show on the watch

--- TaskItWatch WatchKit Extension/InterfaceController.swift
+++ TaskItWatch WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,15 @@ import CoreDataShare
 
 class InterfaceController: WKInterfaceController {
   
+  @IBOutlet weak var table: WKInterfaceTable!
+  var tasks: [AnyObject]!
+  
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
     
     // Configure interface objects here.
+    updateTasks()
+    updateTable()
   }
   
   override func willActivate() {
@@ -26,6 +31,30 @@ class InterfaceController: WKInterfaceController {
   override func didDeactivate() {
     // This method is called when watch view controller is no longer visible
     super.didDeactivate()
+  }
+  
+  func updateTable () {
+    if self.tasks.count != 0 {
+      
+      self.table.setNumberOfRows(self.tasks.count, withRowType: "TaskRowCell")
+      
+      for var index = 0; index < tasks.count; index++ {
+        var theRow = self.table.rowControllerAtIndex(index) as! TaskRow
+        let task = self.tasks[index] as! Task
+        theRow.textLabel.setText(task.titleName)
+        
+        if task.isCompleted == true {
+          theRow.completionButton.setBackgroundColor(TaskHelper.sharedInstance.getDoneColor())
+        } else {
+          theRow.completionButton.setBackgroundColor(TaskHelper.sharedInstance.getNotDoneColor())
+        }
+      }
+      
+    }
+  }
+  
+  func updateTasks() {
+    self.tasks = TaskHelper.sharedInstance.getTasks()
   }
   
 }
---
+++ TaskItWatch WatchKit Extension/TaskRow.swift
@@ -0,0 +1,21 @@
+//
+//  TaskRow.swift
+//  TaskItWatch
+//
+//  Created by Kenneth Wilcox on 5/10/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class TaskRow: NSObject {
+   
+  @IBOutlet weak var completionButton: WKInterfaceButton!
+  @IBOutlet weak var textLabel: WKInterfaceLabel!
+  
+  
+  @IBAction func completionButtonTapped() {
+    
+  }
+}
