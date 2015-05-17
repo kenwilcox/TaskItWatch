f454d073ed0147209b6ca5725ba5dbde3e22e135
Ken Wilcox <wilcoxk@gmail.com>
2015-05-17 15:47:15 -0600

    finally fixed the TODO

--- TaskItWatch/AppDelegate.swift
+++ TaskItWatch/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import CoreData
+import CoreDataShare
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -41,10 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     // Saves changes in the application's managed object context before the application terminates.
-    
-    //TODO: ADD THIS BACK!
-    //FIXME: Something
-//    self.saveContext()
+    CoreDataManager.sharedInstance.saveContext()
   }
   
 }
